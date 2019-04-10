baf811d4700846441c409501032a15691ad030e8
Brian Carroll <briancarroll112@gmail.com>
2019-04-10 18:27:33 -0400

    correct secret key and knock config

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,8 +11,7 @@ module ApplicationCable
       begin
         header_array = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(',')
         token = header_array[header_array.length-1]
-        decoded_token = JWT.decode token.strip, Rails.application.secrets.secret_key_base, true, { :algorithm => 'HS256' }
-        #decoded_token == nil :(
+        decoded_token = JWT.decode token.strip, Rails.application.credentials.fetch(:secret_key_base), true, { :algorithm => 'HS256' }
         if (current_user = User.find((decoded_token[0])['sub']))
           current_user
         else
