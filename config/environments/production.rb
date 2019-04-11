d92b3a68f7a221f22369f66335bb5a93aa6f3ad8
Brian Carroll <briancarroll112@gmail.com>
2019-04-11 17:28:06 -0400

    ws prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://infinite-escarpment-51215.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://infinite-escarpment-51215.herokuapp.com/', 'http://infinite-escarpment-51215.herokuapp.com/']
 end
