8b0e6781f762316cbeb7536aba78dc0f87d5765e
Brian Carroll <briancarroll112@gmail.com>
2019-04-11 12:02:11 -0400

    create room formatting + basic style

--- client/src/components/CreateRoom.jsx
+++ client/src/components/CreateRoom.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
 
 export default (props) => (
-  <div>
-    <h2> Create a Room! </h2>
-    <h3>or choose an existing room from the list</h3>
+  <div className="create-room-container">
     <div>
-      <form onSubmit={(e) => props.handleSubmit(e)}>
-        <label forHtml="name">Room Name</label>
-        <input
-          name="name"
-          id="name"
-          type="text"
-          onChange={(e) => props.handleChange(e)}
-          value={props.name}
-        />
-
-        <label forHtml="description">Description</label>
-        <textarea
-          name="description"
-          id="description"
-          onChange={(e) => props.handleChange(e)}
-          value={props.description}
-        />
-
-        <label forHtml="motd">Message of The Day</label>
-        <input
-          name="motd"
-          id="motd"
-          type="text"
-          onChange={(e) => props.handleChange(e)}
-          value={props.motd}
-        />
-
-        <input type="submit" value="Create" />
-      </form>
+      <h2> Create a Hangout </h2>
+      <h3>or choose an existing room from the list</h3>
     </div>
+    <form onSubmit={(e) => props.handleSubmit(e)}>
+      <input
+        name="name"
+        placeholder="room name"
+        id="name"
+        type="text"
+        onChange={(e) => props.handleChange(e)}
+        value={props.name}
+      />
+
+      <textarea
+        name="description"
+        placeholder="description"
+        id="description"
+        onChange={(e) => props.handleChange(e)}
+        value={props.description}
+      />
+
+      <input
+        name="motd"
+        placeholder="message of the day"
+        id="motd"
+        type="text"
+        onChange={(e) => props.handleChange(e)}
+        value={props.motd}
+      />
+
+      <input type="submit" value="Create" />
+    </form>
   </div>
 )
