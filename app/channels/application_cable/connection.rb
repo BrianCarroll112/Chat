9455ebeb8c1c2eb309a399eefc2f6899d45b1196
Brian Carroll <briancarroll112@gmail.com>
2019-04-13 16:57:46 -0400

    url changes

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,7 +11,7 @@ module ApplicationCable
       begin
         header_array = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(',')
         token = header_array[header_array.length-1]
-        decoded_token = JWT.decode token.strip, '95e750747d929c1d503b409b708fd5ab7e4153a4af8f2a858dd0c151a86df46b485fd8c6b9b1f0cd613982da03c0e4389c1a47e89da060ac932d93374e271afb', true, { :algorithm => 'HS256' }
+        decoded_token = JWT.decode token.strip, Rails.application.credentials.fetch(:secret_key_base), true, { :algorithm => 'HS256' }
         if (current_user = User.find((decoded_token[0])['sub']))
           current_user
         else
--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -36,8 +36,7 @@ Knock.setup do |config|
   ## Configure the key used to sign tokens.
   ##
   ## Default:
-  config.token_secret_signature_key = '95e750747d929c1d503b409b708fd5ab7e4153a4af8f2a858dd0c151a86df46b485fd8c6b9b1f0cd613982da03c0e4389c1a47e89da060ac932d93374e271afb'
-  # config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
 
@@ -47,7 +46,7 @@ Knock.setup do |config|
   ## Configure the public key used to decode tokens, if required.
   ##
   ## Default:
-  config.token_public_key = '95e750747d929c1d503b409b708fd5ab7e4153a4af8f2a858dd0c151a86df46b485fd8c6b9b1f0cd613982da03c0e4389c1a47e89da060ac932d93374e271afb'
+  # config.token_public_key = nil
 
   ## Exception Class
   ## ---------------
